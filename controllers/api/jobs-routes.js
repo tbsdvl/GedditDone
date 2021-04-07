174925d634587faf27d9a1bad3b651eecd8d900d
tbsdvl <tristonburns97@gmail.com>
2021-04-07 15:05:31 -0700

    Adding front end prototype

--- controllers/api/jobs-routes.js
+++ controllers/api/jobs-routes.js
@@ -1,22 +1,20 @@
-const router = require ('express').Router();
-const {Jobs, User} =require('../../models');
+const router = require("express").Router();
+const { Jobs, User } = require("../../models");
 
 // /api/jobs
 //get all jobs
 
-router.get('/', async (req,res)=> {
-    try{
-        const jobsData= await Jobs.findAll({
-            include: [{ model: User}]
-        })
-        const jobs= jobsData.map((job)=>
-        job.get({plain: true}));
-        res.render('jobs', {jobs})
-    } catch (err){
-        console/log(err);
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const jobsData = await Jobs.findAll({
+      include: [{ model: User }],
+    });
+    const jobs = jobsData.map((job) => job.get({ plain: true }));
+    res.render("jobs", { jobs });
+  } catch (err) {
+    console / log(err);
+    res.status(500).json(err);
+  }
 });
-
 
 module.exports = router;
