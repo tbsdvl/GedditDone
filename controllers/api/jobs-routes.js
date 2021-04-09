ea440f29e42d8c4314545a74c0a2ba2623c4674a
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-09 10:54:57 -0700

    routes and views

--- controllers/api/jobs-routes.js
+++ controllers/api/jobs-routes.js
@@ -1,47 +1,101 @@
 const router = require("express").Router();
 const { Jobs, User } = require("../../models");
-
+const withAuth = require('../../utils/auth');
 // /api/jobs
 //get all jobs
 
-// Return all jobs as a json object
-
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     try {
-      const dbJobsData = await Jobs.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['firstname', 'lastname', 'email'],
-          },
-        ],
-      });
-  
-      const jobs = dbJobsData.map((job) =>
-        job.get({ plain: true })
-      );
-  
-      res.render('jobs', {
-        jobs,
-      });
+        const dbJobsData = await Jobs.findAll({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+            attributes: ['jobtitle', 'salary', 'description', 'city', 'state'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['firstname', 'lastname', 'email'],
+                },
+            ],
+        });
+
+        const jobs = dbJobsData.map((job) =>
+            job.get({ plain: true })
+        );
+
+        res.render('jobs', {
+            jobs,
+        });
     } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+        console.log(err);
+        res.status(500).json(err);
     }
-  });
-
-// Return single job as json object based on
-// job id
-router.get('/:id', async (req, res) => {
-  try {
-    const jobsData = await Jobs.findByPk(req.params.id);
-
-    res.status(200).json(jobsData);
-
-  }catch (err) {
-    if (err) throw err;
-    res.status(500).json(err);
-  }
 });
+router.post('/:id', withAuth, async (req, res) => {
+    try {
+        const newJob = await Jobs.create({
+            where: { jobtitle, company, city, state, description, salary} = req.body,
+            user_id: req.session.user_id,
+        
+        });
+        jobtitle=uppercase(job);
+        company = company.toLowerCase().replace(/,[ ]+/g, ",");
+        company=uppercase(company);
+        city = city.toLowerCase().replace(/,[ ]+/g, ",");
+        city=uppercase(company);
+        state = company.toLowerCase().replace(/,[ ]+/g, ",");
+        state=uppercase(company);
+        
+        res.redirect('/jobs');
+        res.status(200).json(newJob);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+router.get('/:id', withAuth, async (req, res) => {
+    try {
+        const jobsData = await Jobs.findByPk({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!jobsData) {
+            res.status(404).json({ message: 'No job found with this id!' });
+            return;
+        }
+
+        res.status(200).json(jobsData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const jobData = await Jobs.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!jobData) {
+            res.status(404).json({ message: 'No job found with this id!' });
+            return;
+        }
+
+        res.status(200).json(jobData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
+
 
 module.exports = router;
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,52 @@
 const newFormHandler = async (event) => {
-  event.preventDefault();
-
-  const jobtitle = document.querySelector('#job-title').value.trim();
-  const salary = document.querySelector('#salary').value.trim();
-  const description = document.querySelector('#job-desc').value.trim();
-
-  if (firstname && lastname && salary && description) {
-    const response = await fetch(`/api/jobs`, {
-      method: 'POST',
-      body: JSON.stringify({ firstname, lastname, salary, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create job posting');
+    event.preventDefault();
+  
+    const jobtitle = document.querySelector('#job-title').value.trim();
+    const salary = document.querySelector('#salary').value.trim();
+    const description = document.querySelector('#job-desc').value.trim();
+    const city = document.querySelector('#theCity').value.trim();
+    const state = document.querySelector('#state').value.trim();
+  
+    if (jobtitle && salary && description && city && state) {
+      const response = await fetch(`/api/jobs`, {
+        method: 'POST',
+        body: JSON.stringify({ jobtitle, salary, description, city, state }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create job posting');
+      }
     }
-  }
-};
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/job/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete job post');
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/job/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete job post');
+      }
     }
-  }
-};
-
-document
-  .querySelector('.new-job-form')
-  .addEventListener('submit', newFormHandler);
-
-document
-  .querySelector('.job-list')
-  .addEventListener('click', delButtonHandler);
+  };
+  
+  document
+    .querySelector('.create-form')
+    .addEventListener('Create', newFormHandler);
+  
+  // document
+  //   .querySelector('.delete-form')
+  //   .addEventListener('DELETE', delButtonHandler);
+  
+  
+    
