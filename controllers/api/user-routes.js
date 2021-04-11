1e0216e7cf61b4160071d111e5c4acae4f7d2c27
Hanna Ferris <hferris@dbq.edu>
2021-04-10 17:26:18 -0700

    passport auth

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,72 +1,48 @@
-const router = require('express').Router();
-const { User } = require('../../models');
-const validator = require('validator');
+const router = require("express").Router();
+const { User } = require("../../models");
+const validator = require("validator");
 const passport = require("../config/passport");
 
+// At the success of login, send user to those who already have account
+module.exports = function (app) {
+  app.post("api/login", passport.authenticate("native"), (req, res) => {
+    res.json({ email: req.user.email, id: req.user.id });
+  });
+};
+// A user's login password is stored and if account is created successfully,then it will login the user.
+app.post("/api/create-account", (req, res) => {
+  Database.User.findOne({ where: { email: req.body.email } }).then(
+    (DatabaseUser) => {
+      // When user with email id is already in the system, then the below message will display.
+      if (!DatabaseUser) {
+        res.status(400).json("The email address is already in use.");
+        return;
+      }
 
+      //User-model data
+      if (!DatabaseUser) {
+        DatabaseUser.User.produce({
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          email: req.body.email,
+          password: req.body.password
+        })
+        //render login page after user signs up.
+        .then(() =>{ res.render("login")})
+        .catch(error => { res.status(401).json(error)});
 
+        //This is how the user signs out.
+        app.get("/signout", (req, res) =>{
+          req.signout(); res.shift("/");
+        });
 
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
+        //Collecting user info that will be used and if the user is not logged in send back blank or send back id and email.
+        app.get("/api/userInfo", (req, res) => {
+          if (!req.user) { res.json({})}
+          else { res.json({ email: req.user.email, id: req.user.id})};
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    // Backup email validator in case code breaks
-    // validator.isEmail(userData);
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
+        });
+        
     }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    // Backup password validator in case code breaks
-    // const validPassword = validator.isStrongPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
     });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
 });
-
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,37 +1,39 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 // Import express-session
-const session = require('express-session');
-const exphbs = require('express-handlebars');
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+const passport = require("../config/passport");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up sessions
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   resave: false,
   saveUninitialized: true,
 };
 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
-
