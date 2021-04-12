16ee1b909574f1d25973e639f7f192331881516a
Hanna Ferris <hferris@dbq.edu>
2021-04-12 13:45:01 -0700

    helper update

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,23 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
-  get_emoji: () => {
-    const randomNum = Math.random();
-    let book = "📗";
-
-    if (randomNum > 0.7) {
-      book = "📘";
-    } else if (randomNum > 0.4) {
-      book = "📙";
-    }
-
-    return `<span for="img" aria-label="book">${book}</span>`;
-  },
-};
-=======
     gt: (context, limit) => {
         if(context > limit){
             return context
         }
     }
 }
->>>>>>> 7c37c00917cb3c4c4e9d87a1c4a75b5f19fe4dbf
